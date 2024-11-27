33c0feb2b0360a04b63014e05dbc42ebecc2e9b4
Gabriel Issamu Oda <biel.oda@gmail.com>
2024-11-26 23:17:38 -0300

    test(startup): adicionando classes de teste iniciais

--- src/test/java/com/gissamusworkspace/cupcakeapi/CupcakeApiApplicationTests.java
+++ src/test/java/com/gissamusworkspace/cupcakeapi/CupcakeApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.gissamusworkspace.cupcakeapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class CupcakeApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
---
+++ src/test/java/com/gissamusworkspace/cupcakeapi/controllers/CupcakeControllerTest.java
@@ -0,0 +1,11 @@
+package com.gissamusworkspace.cupcakeapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class CupcakeControllerTest {
+
+    @Test
+    void testGetEndpoint() {
+    }
+
+}
