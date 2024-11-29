ea7cd230c188a1d3713608ba4c28045a078ba3c0
Gabriel Issamu Oda <biel.oda@gmail.com>
2024-11-28 23:05:26 -0300

    feat(profile): adicionando profiles

--- src/main/java/com/gissamusworkspace/cupcakeapi/CupcakeApiApplication.java
+++ src/main/java/com/gissamusworkspace/cupcakeapi/CupcakeApiApplication.java
@@ -2,8 +2,10 @@ package com.gissamusworkspace.cupcakeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile({"local", "heroku"})
 public class CupcakeApiApplication {
 
 	public static void main(String[] args) {
