7bdab9d23b45758f1de46aec49d89d88b95c2d25
Gabriel Issamu Oda <biel.oda@gmail.com>
2024-12-02 13:57:57 -0300

    fix(token-service): correcao da importacao dos valores utilizados via parametros

--- src/main/java/com/gissamusworkspace/cupcakeapi/services/security/TokenService.java
+++ src/main/java/com/gissamusworkspace/cupcakeapi/services/security/TokenService.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -14,12 +13,11 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
-@RequiredArgsConstructor
 public class TokenService {
 
-    private String secret;
+    private final String secret;
 
-    private String issuer;
+    private final String issuer;
 
     public TokenService(@Value("${cupcakeapi.security.token.secret}") String secret, @Value("${cupcakeapi.security.token.issuer}") String issuer) {
         this.secret = secret;
